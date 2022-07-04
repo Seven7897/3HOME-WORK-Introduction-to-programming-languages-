006d5adbd742051db230152c257196b082288065
Anatoly Finchenko <finchenko777@gmail.com>
2022-07-04 21:58:23 +0300

    *Доделанная_первая_задача*

--- homework001/Program.cs
+++ homework001/Program.cs
@@ -3,20 +3,17 @@ Console.WriteLine("Введите 5-ти значное число :");
 string number = Console.ReadLine();
 int index = 0 ;
 int length = number.Length;
-int and = number.Length - index - 1 ;
 for (int i = 0; i < length / 2; i++)
-{
-    if (number[i] == number[and]) 
-    i++;
-    else
-    Console.WriteLine("Ваше число не является полиндропом");
-}
-Console.WriteLine("Ваше число  является полиндропом");
-
-
-/*
-for (int i = 0; i < length; i++)
-{
+{   int and = number.Length - index - 1 ;
+    if (number[index] == number[and]) 
+    {
     
+    }
+    else
+    {
+    Console.WriteLine("Ваше число не является полиндромом");
+    return;
+    }
+    index++;
 }
-*/
+Console.WriteLine("Ваше число  является полиндромом");
