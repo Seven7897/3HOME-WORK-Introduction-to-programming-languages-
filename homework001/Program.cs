0c3e4476d4bc167888297b18c45ea3f360b18abc
Anatoly Finchenko <finchenko777@gmail.com>
2022-07-04 19:40:48 +0300

    *Первая_задача_полиндроп*

---
+++ homework001/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Введите 5-ти значное число :");
+string number = Console.ReadLine();
+int index = 0 ;
+int length = number.Length;
+int and = number.Length - index - 1 ;
+for (int i = 0; i < length / 2; i++)
+{
+    if (number[i] == number[and]) 
+    i++;
+    else
+    Console.WriteLine("Ваше число не является полиндропом");
+}
+Console.WriteLine("Ваше число  является полиндропом");
+
+
+/*
+for (int i = 0; i < length; i++)
+{
+    
+}
+*/
