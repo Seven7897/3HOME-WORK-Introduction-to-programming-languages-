424c72f59b46b21d6248f72498b5d1d9ee6ffe30
Anatoly Finchenko <finchenko777@gmail.com>
2022-07-04 21:36:53 +0300

    *Вторая_задача*

---
+++ homework002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите 5-ти значное число :");
+string number = Console.ReadLine();
+int index = 0 ;
+int length = number.Length;
+int and = length - 1 - index ;
+while (index < length / 2)
+{
+    if (number[index] == number[and]) 
+    {
+        Console.WriteLine("+");        
+    }
+    else
+    {
+        Console.WriteLine("нет");
+        break;
+    }
+    index++;
+}
